1c12a8abafd1154ae4c5495f770b9c4d6f2091d1
Isaac Boteler <IsaacNAB14@gmail.com>
2025-03-31 23:51:57 -0400

    Provided Calculator.cpp with for parantheses support

--- CppProjects/Calculator/Calculator.cpp
+++ CppProjects/Calculator/Calculator.cpp
@@ -1,32 +1,32 @@
 /*Calculator 
  *See README.md for more info 
  *Current Capabilities: reduction of algebraic expressions of form n : Q|[+,*,/,^]|[n,Q] 
- *Immediate Future: support for common constants (pi,e)
- *Sometime Future: support for parantheses 
+ *Immediate Future: support for common constants support for parantheses 
  *Maybe Future: support for common functions (trig,log,ln) 
  */
  
 #include <iostream> 
 #include <string> 
-#include <cmath>
-
+#include <cmath> 
 
 
 //Function Decl 
-
-long double Reducer(const std::string&);
-long double NumFromString(const std::string&,int&); 
+size_t EndOfExp(const std::string& input,size_t st); 
+long double Reducer(const std::string& input,size_t& st,size_t en);
+long double NumFromString(const std::string& input,size_t& st); 
 
 
 //Runner Code 
 int main() {
 	std::string input = "NOT EXIT";
+	size_t st, en; 
 	while (input != "EXIT") {
 		std::cout << "Please input expression to reduce" << std::endl; 
 		std::cin >> input;
-	       	
+	       	st = 0;
+		en = input.length();
 		std::cout << "...\n...\n...\n"
-			  << Reducer(input) << std::endl; 			
+			  << Reducer(input,st,en) << std::endl; 			
 		std::cout << "EXIT?" << std::endl; 
 		std::cin >> input; 		
 	}
@@ -36,77 +36,78 @@ int main() {
 
 
 
-//Func Def 
+//This is an attempt to redo Reducer() with added parantheses functionality 
+//I will be using recursion, the aim is to eventually redesign this into dynamic looping  
+long double Reducer(const std::string& input,size_t& st,size_t en) {
+	
+	long double ans = 0; 
 
-//Function is designed to store little extra information 
-//We reduce from left to right with ans being our final answer 
-//Read a number
-//Check if number is raised to a power
-	//reduce the Q^Q situation to Q 
-//Check if number is multiplied to another 
-	//Check if that number is raised to a power 
-		//reduce the Q^Q situation to Q 
-	//Reduce the Q*Q situation to Q 
-//Add Q to ans 
-//Repeat until end of input
-
-//Subtraction is represented as the addition of a negative : a - b = a + -b
-long double Reducer(const std::string& input) {
-	int len = input.length(); 
-	long double ans = 0;  
-	int index = 0; 
-	while (index < len) {
-		long double number = NumFromString(input,index); //This is what we are adding to ans  
-		//This grabs the first number from the string 
+	while (st < en) {
+		if (input[st] == ')') 
+			st++; //Pesky Parantheses 
 		
-		//We do this like so because I am dumb 
-		while (input[index] == '^') { 
-			index++; //We iterate past the operator  
-			//This is our power layer 
-			double numberPow = NumFromString(input,index); //This is what we are exponentiating to number  
-			//No more layers so we start the result chain
-			number = pow(number,numberPow);
+		long double number = NumFromString(input,st);
+
+
+		while (input[st] == '^') {
+			st++; //Moving st past operator 
+			long double numberPow = NumFromString(input,st);
 			
-			    	
+			number = std::pow(number,numberPow);
 		}
-		while (input[index] == '*' || input[index] == '/') {
-			bool multiplication = (input[index] == '*')? 1 : 0;
-			index++; //We iterate past the operator 
 
-			//This is our multiplication layer
-			long double numberMult = NumFromString(input,index); //This is what we are multiplying to number 
-			
-
-			while (input[index] == '^') { 
-				index++; //We iterate past the operator  
-				//This is our power layer 
-				double numberPow = NumFromString(input,index); //This is what we are exponentiating to numberMult  
-				//No more layers so we start the result chain
-				numberMult = pow(numberMult,numberPow);
-			    	
+		while (input[st] == '*' || input[st] == '/') {
+			bool isMult = (input[st] == '*')? true : false; 
+			st++; //Moving st past operator
+			long double numberMult = NumFromString(input,st);
+			while (input[st] == '^') {
+				st++; //Moving st past operator 
+				long double numberPow = NumFromString(input,st);
+				
+				numberMult = std::pow(numberMult,numberPow);
 			}
-			
-			if (multiplication) 
+			if (isMult) 
 				number *= numberMult;
 			else 
 				number /= numberMult;	
 		}
-		ans += number;
-		index++; //We iterate past the operator 
-	}
+		ans += number; 
+		st++; //Moving st past operator 
+	} 
 	return ans; 
-} 
+}
 
 
-long double NumFromString(const std::string& input,int& index) { 
-	int stOfNum = index;
-      	if (input[index] == '-') 
-		index++; 	
-	while ( (input[index] >= '0' && input[index] <= '9') || 
-		(input[index] == '.' || input[index] == ',') ) {
-		
-		index++; 
+long double NumFromString(const std::string& input,size_t& st) { 
+	size_t stOfNum = st; 
+	size_t enOfNum; 
+	if (input[stOfNum] == '(') {
+		enOfNum = EndOfExp(input,st);
+		st++;
+		return Reducer(input,st,enOfNum);
 	}
-	 
-	return std::stold( input.substr(stOfNum,index - stOfNum) );	
+	if (input[st] == '-') 
+		st++; 	
+	while ( (input[st] >= '0' && input[st] <= '9') || 
+		(input[st] == '.' || input[st] == ',') ) {		
+		st++; 
+	}
+	
+	
+	return std::stold( input.substr(stOfNum,st - stOfNum) );	
+}
+size_t EndOfExp(const std::string& input,size_t st) {
+	//We assume the expression doesn't have mismatched parantheses 
+	
+	int balance = 1; //The Difference between # of '(' and ')' 
+	while (balance != 0) {
+	       	st++;	
+		if (input[st] == '(') {
+			balance++;
+		} else if (input[st] == ')') {
+			balance--; 
+		}
+	}
+
+	return st; 
 }
